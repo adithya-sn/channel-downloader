3118aa5459db641f1e3cfa8a2b105a383491a47e
José Henrique Ivanchechen <joseivanchechen@outlook.com>
2019-02-17 21:24:28 -0300

    add more url support

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 1. Make sure you have python, chromedriver and ffmpeg installed.
 2. Install youtube-dl and selenium, if you don't already have them, using `pip install selenium youtube-dl`.
 3. Download or clone this git repo, using `git clone https://github.com/joseivanchechen/channel-downloader`
-4. Once you're inside the folder which contains the `main.py` file, run `python main.py <user name>` to start downloading.
-> Note: replace `user name` with the name that's on the URL of the youtube channel videos tab.
+4. Once you're inside the folder which contains the `main.py` file, run `python main.py <username/channel id/url>` to start downloading.
 
 It will create a directory named `downloads`, containing folders with the name of the channels that you've downloaded.
--- main.py
+++ main.py
@@ -13,22 +13,47 @@ if not os.path.exists("downloads/"):
     print("[Channel Downloader] Downloads folder doesn't exist, creating one.")
     os.makedirs("downloads/")
 
-if not os.path.exists("downloads/%s/" % (channel)):
-    os.makedirs("downloads/%s/" % (channel))
+print("[Channel Downloader] Starting download on '%s'." % channel)
 
-print("[Channel Downloader] Starting download on youtube channel '%s'." % channel)
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 browser = webdriver.Chrome(chrome_options=chrome_options)
 
-url = "https://www.youtube.com/user/%s/videos" % (channel)
+url = ""
+
+if("/channel/" in channel):
+    # Get youtube channel name from videos tab
+    browser.get(channel) # It's a link
+    browser.find_element_by_xpath("//*[@id=\"tabsContent\"]/paper-tab[2]/div").click()
+    time.sleep(2)
+    url = browser.current_url
+    channel = url.split("/")[-2]
+elif("/user/" in channel):
+    rurl = channel.split("/")
+    url = ""
+    for p in rurl[:-1]:
+        url += p+"/"
+    url += "videos"
+    channel = url.split("/")[-2]
+else:
+    url = "https://youtube.com/user/%s/videos" % (channel)
+print(url)
+print("[Channel Downloader] Loading entire channel page...")
+
 browser.get(url)
 time.sleep(3)
 source = browser.page_source
 if(len(source) < 100000): # Channel doesn't exist
-    print("[Channel Downloader] URL: %s" % (url))
-    sys.exit("[Channel Downloader] There was an error while loading the channel at the URL above, check if it exists. Exiting.")
+    url = "https://youtube.com/channel/%s/videos" % (channel)
+    browser.get(url)
+    time.sleep(3)
+    source = browser.page_source
+    if(len(source)< 100000):
+        print("[Channel Downloader] URL: %s" % (url))
+        sys.exit("[Channel Downloader] There was an error while loading the channel at the URL above, check if it exists. Exiting.")
 
+if not os.path.exists("downloads/%s/" % (channel)):
+    os.makedirs("downloads/%s/" % (channel))
 
 # Get to the bottom of the page, so all videos will load into the source code
 lsource = len(source)
@@ -41,6 +66,7 @@ while True:
     lsource = nsource
 
 
+print("[Channel Downloader] Fetching video URLs...")
 # Parse the HTML to get the video URLs
 source = browser.page_source
 browser.close()
@@ -83,4 +109,4 @@ ytd_options = {
 with youtube_dl.YoutubeDL(ytd_options) as dl:
     dl.download(videos)
 
-print("[Channel Downloader] Finished downloading %d videos from '%s'!" % (size, channel))
+print("[Channel Downloader] Finished download %d videos from '%s'!" % (size, channel))
